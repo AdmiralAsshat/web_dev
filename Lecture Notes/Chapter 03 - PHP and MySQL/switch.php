bb756e7c53e06dee507ab4cf5e379927d0935c7d
AdmiralAsshat <AsshatAdmiral@gmail.com>
2014-12-20 16:31:50 -0600

    Lecture 113: Switch

---
+++ Lecture Notes/Chapter 03 - PHP and MySQL/switch.php
@@ -0,0 +1,18 @@
+<?php
+
+	$name = "Fred";
+
+	switch($name)
+	{
+		case "":
+			echo "No value for variable name.";
+			break;
+		case "Bob":
+			echo "Your name is Bob.";
+			break;
+		default:
+			echo "Invalid input.";
+
+	}
+
+?>
