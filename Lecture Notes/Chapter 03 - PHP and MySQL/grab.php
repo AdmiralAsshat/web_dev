80be0b860b37def80d767d3eaa37514937004bfc
AdmiralAsshat <AsshatAdmiral@gmail.com>
2014-12-20 21:03:26 -0600

    Lecture 115: Post, Get, Request

---
+++ Lecture Notes/Chapter 03 - PHP and MySQL/grab.php
@@ -0,0 +1,12 @@
+
+<?php
+
+$first_name = $_GET['fname'];
+$last_name = $_GET['lname'];
+$secret = $_REQUEST['secret_pass'];
+
+echo "The first name is: ".$first_name."<br />";
+echo "The last name is: ".$last_name."<br />";
+echo "The secret information is ".$secret."<br />";
+
+?>
