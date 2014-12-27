65cbd2f64f320040f5cbe264c9369934e0525bad
AdmiralAsshat <AsshatAdmiral@gmail.com>
2014-12-26 23:00:12 -0600

    Lecture 135: Passing Values Through URL

--- Final Project - PHP and MySQL/webproject1/delete.php
+++ Final Project - PHP and MySQL/webproject1/delete.php
@@ -6,9 +6,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
   include('connection.php');
   
-  $getemail = mysqli_real_escape_string($dbc, trim($_POST['getemail']));
+  $id_user= mysqli_real_escape_string($dbc, trim($_POST['user_id']));
   
-  mysqli_query($dbc, "DELETE FROM users WHERE email='$getemail'");
+  mysqli_query($dbc, "DELETE FROM users WHERE id='$id_user'");
   
   echo "The user has been successfully deleted.";
   
@@ -18,8 +18,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 
 ?>
 
-<h3>Type the email of the user to be deleted</h3>
+<h3>Are you sure you want to delete this user?</h3>
 <form method="post" action="delete.php">
-  <p><input type='text' name='getemail' /></p>
-  <p><input type='submit' name='submit' value='Delete This User' /></p>
+  <p>User ID:<input type='text' name='user_id' value="<?php if (!empty($_GET['user_id'])) { echo $_GET['user_id'];} ?>"/></p>
+  <p>First Name:<input type='text' name='first_name' value="<?php if (!empty($_GET['fname'])) { echo $_GET['fname'];} ?>"/></p>
+  <p>Last Name:<input type='text' name='last_name' value="<?php if (!empty($_GET['lname'])) { echo $_GET['lname'];} ?>"/></p>
+  <p><input type='submit' name='submit' value='Yes' /></p>
+  <p><a href="output.php">Go Back</a></p>
 </form>
