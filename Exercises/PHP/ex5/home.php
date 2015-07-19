966fb75ba4abfc81291ad0e75474ee06f60816a1
AdmiralAsshat <AsshatAdmiral@gmail.com>
2015-07-19 14:26:06 -0400

    Start PHP Chapter

---
+++ Exercises/PHP/ex5/home.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Home Page</title>
+  </head>
+  
+  <body>
+    <h1><p><center>Welcome to the World of Freelancing</center></h1></p>
+    
+    <h3><center>
+      <a href="registration_form.php">Sign Up</a>
+      <a href="login_form.php">Login</a>
+    </center></h3>
+  </body>
+</html>
---
+++ Exercises/PHP/ex5/registration_form.php
@@ -0,0 +1,39 @@
+<html>
+  <head>
+    <title>Register</title>
+  </head>
+  
+  <body>
+  
+    <h2>Enter the Details to Register</h2>
+    <form method="post" action="register.php">
+      <table border="0" width="100%">
+        <tr>
+          <td width="10%">First Name: </td>
+          <td><input type="text" id="fname" name="fname" maxlength="15"/></td>
+        </tr>
+        <tr>
+          <td width="10%">Last Name: </td>
+          <td><input type="text" id="lname" name="lname" maxlength="15"/></td>
+        </tr>
+        <tr>
+          <td width="10%">Email: </td>
+          <td><input type="text" id="email" name="email" maxlength="30"/></td>
+        </tr>
+        <tr>
+        <tr>
+          <td width="10%">User ID: </td>
+          <td><input type="text" id="uid" name="uid" maxlength="15"/></td>
+        </tr>
+          <td width="10%">Password: </td>
+          <td><input type="password" id="pass" name="pass" maxlength="15"/></td>
+        </tr>
+        </tr>
+          <td width="10%">Confirm Password: </td>
+          <td><input type="password" id="confpass" name="confpass" maxlength="15"/></td>
+        </tr>
+      </table>
+    </form>
+  
+  </body>
+</html>
