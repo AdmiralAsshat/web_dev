cd99266cbf557e3d631ac531cffcc087b38992de
AdmiralAsshat <asshatadmiral@gmail.com>
2015-09-13 13:16:42 -0400

    Lecture 115
    The querySelectorAll

--- Lecture Notes/Chapter 06 - HTML5/javascript.js
+++ Lecture Notes/Chapter 06 - HTML5/javascript.js
@@ -1,6 +1,10 @@
 
 function bringit(){
-	document.querySelector('#first').onclick=say;
+/*	document.querySelector('#first').onclick=say; */
+	var list = document.querySelectorAll('#first');
+	for(var i=0; i < list.length; i++){
+		list[i].onclick=say;
+	}
 }
 
 function say(){
