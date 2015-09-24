542fd35658e4263369995874f8969f4eb0e319ed
AdmiralAsshat <AsshatAdmiral@gmail.com>
2015-09-23 21:14:46 -0400

    Lecture 209: Request to PHP - Pt. 3

--- Lecture Notes/Chapter 07 - AJAX/login.php
+++ Lecture Notes/Chapter 07 - AJAX/login.php
@@ -7,20 +7,26 @@ if($name && $password){
 	mysql_connect("localhost","root","") or die("Could not connect to database.");
 	mysql_select_db("testlogin");
 
-	$query = mysql_query("SELECT * FROM users WHERE name='".$name."'");
+	$query = mysql_query("SELECT * FROM users WHERE username='".$name."'");
 	$numrows = mysql_num_rows($query);
 
 	if($numrows != 0){
 		while($row = mysql_fetch_assoc($query)){
-			$dbname = $row['name'];
+			$dbname = $row['username'];
 			$dbpassword = $row['password'];
 		}
 
 		if($name==$dbname){
 			if($password==$dbpassword){
-				
+				echo "You are in!";
+			}else{
+				echo "Wrong password!";
 			}
+		}else{
+			echo "Username not found in database.";
 		}
+	}else{
+		echo "This name is not registered";
 	}
 }else{
 	echo "You must type a username and password!";
